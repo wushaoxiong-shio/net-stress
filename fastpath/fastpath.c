e005484f85113ada8c0cc70a53c91ffb8aacfbac
shio <wushaoxiong.shio@gmail.com>
2025-01-24 12:00:14 +0800

    init

--- fastpath/fastpath.c
+++ fastpath/fastpath.c
@@ -1,249 +1,115 @@
 #include "en.h"
+#include "fs_core.h"
 #include <linux/module.h>
 #include <linux/mlx5/fs.h>
 #include "en/fs.h"
+#include "en/port.h"
 
-#include "fastpath.h"
+struct mlx5_fc_cache {
+	u64 packets;
+	u64 bytes;
+	u64 lastuse;
+};
 
 
-struct mlx5e_priv *priv_eth1 = NULL;
-struct mlx5e_priv *priv_eth2 = NULL;
+struct mlx5_fc {
+	struct list_head list;
+	struct llist_node addlist;
+	struct llist_node dellist;
 
-struct mlx5e_conntrack_table *cht = NULL;
+	u64 lastpackets;
+	u64 lastbytes;
+
+	struct mlx5_fc_bulk *bulk;
+	u32 id;
+	bool aging;
+
+	struct mlx5_fc_cache cache ____cacheline_aligned_in_smp;
+};
+
+struct mlx5_fc *fc = NULL;
+struct mlx5e_priv *priv = NULL;
+struct mlx5_flow_table *ft = NULL;
+struct mlx5_flow_handle *rule = NULL;
+
+struct mlx5e_hairpin *
+mlx5e_hairpin_create(struct mlx5e_priv *priv, struct mlx5_hairpin_params *params,
+		     int peer_ifindex);
 
 int init_mlx5_priv(void)
 {
     struct net_device *netdev = dev_get_by_name(&init_net, "eth1");
     if (netdev)
-        priv_eth1 = netdev_priv(netdev);
-
-    netdev = NULL;
-    netdev = dev_get_by_name(&init_net, "eth2");
-    if (netdev)
-        priv_eth2 = netdev_priv(netdev);
-
-    if (!priv_eth1 || !priv_eth2)
-        return -1;
+        priv = netdev_priv(netdev);
 
     return 0;
 }
 
 
-static void mlx5e_destroy_groups_cp(struct mlx5e_flow_table *ft)
-{
-	int i;
-
-	for (i = ft->num_groups - 1; i >= 0; i--) {
-		if (!IS_ERR_OR_NULL(ft->g[i]))
-			mlx5_destroy_flow_group(ft->g[i]);
-		ft->g[i] = NULL;
-	}
-	ft->num_groups = 0;
-}
-
-static void mlx5e_destroy_flow_table_cp(struct mlx5e_flow_table *ft)
-{
-	mlx5e_destroy_groups_cp(ft);
-	kfree(ft->g);
-	mlx5_destroy_flow_table(ft->t);
-	ft->t = NULL;
-}
-
-int create_flow_table(struct mlx5e_conntrack_table *cht)
+static int __init conntrack_fastpath_init(void)
 {
     int err = 0;
-    mlx5_create_flow_table_handler_ptr handler;
 
-    cht->ft_attr.flags = MLX5_FLOW_TABLE_UNMANAGED;
-    cht->ft_attr.max_fte = MLX5E_FS_CONNTRACK_TABLE_SIZE;
-	cht->ft_attr.level = MLX5E_CONNTRACK_FT_LEVEL;
-	cht->ft_attr.prio = MLX5E_NIC_PRIO;
+    init_mlx5_priv();
+    struct mlx5_flow_namespace *ns;
+    struct mlx5_flow_table_attr ft_attr;
 
-    cht->ns = mlx5_get_flow_namespace(priv_eth1->mdev, MLX5_FLOW_NAMESPACE_KERNEL);
+    ft_attr.max_fte = 0;
+    ft_attr.level = 0;
+    ft_attr.prio = 0;
+    ft_attr.autogroup.max_num_groups = 4;
+    ft_attr.autogroup.num_reserved_entries = 2;
 
-    rcu_read_lock();
-    handler = rcu_dereference(mlx5_create_flow_table_handler);
-    if (!handler)
+    ns = mlx5_get_flow_namespace(priv->mdev, MLX5_FLOW_NAMESPACE_KERNEL);
+    ft = mlx5_create_auto_grouped_flow_table(ns, &ft_attr);
+    if (IS_ERR(ft))
     {
-        printk("mlx5_create_ft_handler is NULL!\n");
-        rcu_read_unlock();
-        return -EINVAL;
+        printk("PTR_ERR(ft):%ld\n", PTR_ERR(ft));
+        goto err;
     }
-    rcu_read_unlock();
 
-    cht->ft.t = handler(cht->ns, &cht->ft_attr);
-    if (IS_ERR(cht->ft.t))
-    {
-		err = PTR_ERR(cht->ft.t);
-		cht->ft.t = NULL;
-        return err;
-	}
+    printk("\t mlx5e_create_flow_steering\n");
+    printk("\t--\t ft->id:%x\n", ft->id);
+    printk("\t--\t ft->ns:%x\n", ft->ns);
+    printk("\t--\t ft->level:%d\n", ft->level);
+    printk("\t--\t ft->max_fte:%d\n", ft->max_fte);
+    printk("\t--\t ft->type:%d\n", ft->type);
+    printk("\t--\t ft->flags:%u\n", ft->flags);
+    printk("\t--\t ft->vport:%u\n", ft->vport);
 
-    printk("Created fs def table id %x level %u max_fte %u\n", cht->ft.t->id, cht->ft.t->level, cht->ft.t->max_fte);
-
-    return 0;
-}
-
-int create_table_groups(struct mlx5e_conntrack_table *cht)
-{
-    int err = 0;
-    int ix = 0;
-	u32 *in;
-	u8 *mc;
-    void *outer_headers_c;
-    mlx5_create_flow_group_handler_ptr handler;
-
-    int inlen = MLX5_ST_SZ_BYTES(create_flow_group_in);
-
-    rcu_read_lock();
-    handler = rcu_dereference(mlx5_create_flow_group_handler);
-    if (!handler)
-    {
-        printk("mlx5_create_ft_handler is NULL!\n");
-        rcu_read_unlock();
-        return -EINVAL;
-    }
-    rcu_read_unlock();
-
-    cht->ft.g = kcalloc(MLX5E_FS_CONNTRACK_NUM_GROUPS, sizeof(cht->ft.g), GFP_KERNEL);
-    in = kvzalloc(inlen, GFP_KERNEL);
-    if  (!in || !cht->ft.g)
-    {
-		kfree(cht->ft.g);
-		cht->ft.g = NULL;
-		kvfree(in);
-		return -ENOMEM;
-	}
-
-    mc = MLX5_ADDR_OF(create_flow_group_in, in, match_criteria);
-    outer_headers_c = MLX5_ADDR_OF(fte_match_param, mc, outer_headers);
-
-    MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, ethertype);
-    // MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, ip_protocol);
-    MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, ip_version);
-
-    MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, src_ipv4_src_ipv6.ipv4_layout.ipv4);
-    // MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, dst_ipv4_dst_ipv6.ipv4_layout.ipv4);
-
-    // MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, tcp_sport);
-    // MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, tcp_dport);
-    // MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, udp_sport);
-    // MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, udp_dport);
-
-    MLX5_SET_CFG(in, match_criteria_enable, MLX5_MATCH_OUTER_HEADERS);
-
-    MLX5_SET_CFG(in, start_flow_index, ix);
-    ix += MLX5E_FS_CONNTRACK_GROUP1_SIZE;
-    MLX5_SET_CFG(in, end_flow_index, ix - 1);
-
-    cht->ft.g[cht->ft.num_groups] = handler(cht->ft.t, in);
-    if (!cht->ft.g[0])
-        printk("!cht->ft.g[0]\n");
-
-    if (IS_ERR(cht->ft.g[cht->ft.num_groups]))
-        goto destroy_groups;
-
-    cht->ft.num_groups++;
-
-    memset(in, 0, inlen);
-	MLX5_SET_CFG(in, start_flow_index, ix);
-	ix += MLX5E_FS_CONNTRACK_GROUP2_SIZE;
-	MLX5_SET_CFG(in, end_flow_index, ix - 1);
-	cht->ft.g[cht->ft.num_groups] = handler(cht->ft.t, in);
-	if (IS_ERR(cht->ft.g[cht->ft.num_groups]))
-		goto destroy_groups;
-
-	cht->ft.num_groups++;
-
-	kvfree(in);
-    return 0;
-
-destroy_groups:
-	err = PTR_ERR(cht->ft.g[cht->ft.num_groups]);
-	cht->ft.g[cht->ft.num_groups] = NULL;
-	kvfree(in);
-
-    return err;
-}
-
-int add_default_rule(struct mlx5e_conntrack_table *cht)
-{
-    int err = 0;
-    mlx5_add_flow_rules_handler_ptr handler;
-
-    rcu_read_lock();
-    handler = rcu_dereference(mlx5_add_flow_rules_handler);
-    if (!handler)
-    {
-        printk("mlx5_create_ft_handler is NULL!\n");
-        rcu_read_unlock();
-        return -EINVAL;
-    }
-    rcu_read_unlock();
-
-    MLX5_DECLARE_FLOW_ACT(flow_act);
-    flow_act.action = MLX5_FLOW_CONTEXT_ACTION_ALLOW;
-    flow_act.flags = FLOW_ACT_NO_APPEND;
-
-    cht->default_rule = handler(cht->ft.t, NULL, &flow_act, NULL, 1);
-    if (IS_ERR(cht->default_rule))
-        return PTR_ERR(cht->default_rule);
-
-    return err;
-}
-
-int add_test_rule(struct mlx5e_conntrack_table *cht)
-{
-    int err = 0;
-    struct mlx5_flow_destination dest = {};
-    struct mlx5_flow_spec specs = {};
-    struct mlx5_flow_spec *spec = NULL;
-
+    printk("\t--\t ft->autogroup.active:%d\n", ft->autogroup.active);
+    printk("\t--\t ft->autogroup.group_size:%d\n", ft->autogroup.group_size);
+    printk("\t--\t ft->autogroup.num_groups:%d\n", ft->autogroup.num_groups);
+    printk("\t--\t ft->autogroup.max_fte:%d\n", ft->autogroup.max_fte);
+    printk("\t--\t ft->autogroup.required_groups:%d\n", ft->autogroup.required_groups);
+   
     u32 src_ip = 0x0A010101;
-    // u8 dst_mac[] = {0x50, 0x6b, 0x4b, 0xef, 0xfc, 0x19};
-
-
-    mlx5_add_flow_rules_handler_ptr handler;
-
-    rcu_read_lock();
-    handler = rcu_dereference(mlx5_add_flow_rules_handler);
-    if (!handler)
+    struct mlx5_flow_act flow_act =
     {
-        printk("mlx5_create_ft_handler is NULL!\n");
-        rcu_read_unlock();
-        return -EINVAL;
-    }
-    rcu_read_unlock();
-
-    MLX5_DECLARE_FLOW_ACT(flow_act);
-    flow_act.action = MLX5_FLOW_CONTEXT_ACTION_MOD_HDR | MLX5_FLOW_CONTEXT_ACTION_FWD_DEST;
-    flow_act.flags = FLOW_ACT_NO_APPEND;
-    flow_act.fg = cht->ft.g[0];
-
-    specs.match_criteria_enable = MLX5_MATCH_OUTER_HEADERS;
-    spec = &specs;
+        .action = MLX5_FLOW_CONTEXT_ACTION_COUNT | MLX5_FLOW_CONTEXT_ACTION_FWD_DEST | MLX5_FLOW_CONTEXT_ACTION_MOD_HDR,
+        // .action = MLX5_FLOW_CONTEXT_ACTION_COUNT | MLX5_FLOW_CONTEXT_ACTION_FWD_DEST,
+        .flags  = FLOW_ACT_NO_APPEND,
+    };
+    struct mlx5_flow_spec specd = {};
+    struct mlx5_flow_spec *spec = &specd;
+    struct mlx5_flow_destination dest[2] = {};
+    fc = mlx5_fc_create(priv->mdev, true);
 
     void *headers_c = MLX5_ADDR_OF(fte_match_param, spec->match_criteria, outer_headers);
-	void *headers_v = MLX5_ADDR_OF(fte_match_param, spec->match_value, outer_headers);
-
-    MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, headers_c, ethertype);
-    MLX5_SET(fte_match_set_lyr_2_4, headers_v, ethertype, ETH_P_IP);
+    void *headers_v = MLX5_ADDR_OF(fte_match_param, spec->match_value, outer_headers);
 
     MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, headers_c, ip_version);
     MLX5_SET(fte_match_set_lyr_2_4, headers_v, ip_version, 4);
 
+    MLX5_SET(fte_match_set_lyr_2_4, headers_c, cvlan_tag, 1);
+
     MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, headers_c, src_ipv4_src_ipv6.ipv4_layout.ipv4);
     memcpy(MLX5_ADDR_OF(fte_match_set_lyr_2_4, headers_v, src_ipv4_src_ipv6.ipv4_layout.ipv4), &src_ip, sizeof(src_ip));
 
+    specd.match_criteria_enable = MLX5_MATCH_OUTER_HEADERS;
 
-    for (int i=0; i<112; i++)
-    {
-        printk("match_criteria: %u - %u\n", spec->match_criteria[i], cht->ft.g[0]->mask.match_criteria[i]);
-    }
-
-
-    void *modify_actions = kcalloc(1, MLX5_MH_ACT_SZ * 2, GFP_KERNEL);;
-    memset(modify_actions, 0, MLX5_MH_ACT_SZ * 2);
+    void *modify_actions = kcalloc(1, MLX5_MH_ACT_SZ * 3, GFP_KERNEL);;
+    memset(modify_actions, 0, MLX5_MH_ACT_SZ * 3);
 
     MLX5_SET(set_action_in, modify_actions, action_type, MLX5_ACTION_TYPE_SET);
     MLX5_SET(set_action_in, modify_actions, field, MLX5_ACTION_IN_FIELD_OUT_DMAC_47_16);
@@ -257,127 +123,82 @@ int add_test_rule(struct mlx5e_conntrack_table *cht)
     MLX5_SET(set_action_in, modify_actions + MLX5_MH_ACT_SZ, length, 16);
     MLX5_SET(set_action_in, modify_actions + MLX5_MH_ACT_SZ, data, ntohs(0xfc19));
 
-    flow_act.modify_hdr = mlx5_modify_header_alloc(mlx5e_fs_get_mdev(priv_eth1->fs), MLX5_FLOW_NAMESPACE_KERNEL,
-                                              2, modify_actions);
+    MLX5_SET(set_action_in, modify_actions + MLX5_MH_ACT_SZ * 2, action_type, MLX5_ACTION_TYPE_SET);
+    MLX5_SET(set_action_in, modify_actions + MLX5_MH_ACT_SZ * 2, field, MLX5_ACTION_IN_FIELD_OUT_SIPV4);
+    MLX5_SET(set_action_in, modify_actions + MLX5_MH_ACT_SZ * 2, offset, 0);
+    MLX5_SET(set_action_in, modify_actions + MLX5_MH_ACT_SZ * 2, length, 32);
+    MLX5_SET(set_action_in, modify_actions + MLX5_MH_ACT_SZ * 2, data, ntohl(0x02020202));
+
+    flow_act.modify_hdr = mlx5_modify_header_alloc(mlx5e_fs_get_mdev(priv->fs), MLX5_FLOW_NAMESPACE_KERNEL,
+                                              3, modify_actions);
     if (IS_ERR(flow_act.modify_hdr))
     {
+        err = PTR_ERR(flow_act.modify_hdr);
         printk("mlx5_modify_header_alloc failed: %ld\n", PTR_ERR(flow_act.modify_hdr));
-        return PTR_ERR(flow_act.modify_hdr);
+        goto err;
     }
 
-    dest.type = MLX5_FLOW_DESTINATION_TYPE_PORT;
-    dest.vport.num = mlx5_core_native_port_num(priv_eth2->mdev);
+    struct mlx5_hairpin_params params;
+    struct mlx5e_hairpin *hp;
+    u64 link_speed64;
+	u32 link_speed;
 
-    printk("mlx5_core_native_port_num:%d\n", mlx5_core_native_port_num(priv_eth1->mdev));
-    printk("mlx5_core_native_port_num:%d\n", mlx5_core_native_port_num(priv_eth2->mdev));
+    params.log_data_size = 16;
+	params.log_data_size = min_t(u8, params.log_data_size,
+				     MLX5_CAP_GEN(priv->mdev, log_max_hairpin_wq_data_sz));
+	params.log_data_size = max_t(u8, params.log_data_size,
+				     MLX5_CAP_GEN(priv->mdev, log_min_hairpin_wq_data_sz));
 
-    cht->rules[0] = handler(cht->ft.t, spec, &flow_act, &dest, 1);
-    if (IS_ERR(cht->rules[0]))
+	params.log_num_packets = params.log_data_size -
+				 MLX5_MPWRQ_MIN_LOG_STRIDE_SZ(priv->mdev);
+	params.log_num_packets = min_t(u8, params.log_num_packets,
+				       MLX5_CAP_GEN(priv->mdev, log_max_hairpin_num_packets));
+
+	params.q_counter = priv->q_counter;
+	mlx5e_port_max_linkspeed(priv->mdev, &link_speed);
+	link_speed = max_t(u32, link_speed, 50000);
+	link_speed64 = link_speed;
+	do_div(link_speed64, 50000);
+	params.num_channels = link_speed64;
+
+	hp = mlx5e_hairpin_create(priv, &params, 4);
+
+    dest[0].type = MLX5_FLOW_DESTINATION_TYPE_TIR;
+    dest[0].tir_num = 29;
+    dest[1].type = MLX5_FLOW_DESTINATION_TYPE_COUNTER;
+    dest[1].counter_id = mlx5_fc_id(fc);
+
+    rule = mlx5_add_flow_rules(ft, spec, &flow_act, dest, 2);
+    if (IS_ERR(rule))
     {
-        printk("IS_ERR(cht->rules[0])\n");
-        return PTR_ERR(cht->rules[0]);
+        printk("\t - %ld\n", PTR_ERR(rule));
+        goto err;
     }
 
-    cht->num_rules++;
-    return err;
-}
+    return 0;
 
-void del_flow_rules(struct mlx5_flow_handle *h)
-{
-    mlx5_del_flow_rules_handler_ptr handler;
-
-    rcu_read_lock();
-    handler = rcu_dereference(mlx5_del_flow_rules_handler);
-    if (!handler)
-    {
-        printk("mlx5_del_flow_rules_handler is NULL!\n");
-        rcu_read_unlock();
-        return ;
-    }
-    rcu_read_unlock();
-
-    handler(h);
-    return ;
-}
-
-static void destroy_mlx5e_conntrack_table(struct mlx5e_conntrack_table *cht)
-{
-    del_flow_rules(cht->default_rule);
-    for (int i = 0; i < cht->num_rules; i++)
-    {
-        printk("cht->num_rules:%d i:%d\n", cht->num_rules, i);
-        del_flow_rules(cht->rules[i]);
-    }
-
-	mlx5e_destroy_flow_table_cp(&cht->ft);
-
-    kfree(cht);
-    return ;
-}
-
-static int __init conntrack_fastpath_init(void)
-{
-    int err = 0;
-
-    err = request_module("mlx5_core");
-    if (err < 0)
-    {
-        printk("request module:mlx5_core!\n");
-        return err;
-    }
-    
-    err = init_mlx5_priv();
-    if (err)
-    {
-        printk("file:%s line:%d init_mlx5_priv err:%d\n", __FILE__, __LINE__, err);
-        return err;
-    }
-
-    cht = kcalloc(1, sizeof(struct mlx5e_conntrack_table), GFP_KERNEL);
-    if (!cht)
-    {
-        printk("kcalloc failed!\n");
-        return -1;
-    }
-
-    err = create_flow_table(cht);
-    if (err)
-    {
-        printk("file:%s line:%d create_flow_table err:%d\n", __FILE__, __LINE__, err);
-        return err;
-    }
-
-    err = create_table_groups(cht);
-    if (err)
-    {
-        printk("file:%s line:%d create_table_groups err:%d\n", __FILE__, __LINE__, err);
-        return err;
-    }
-
-    err = add_default_rule(cht);
-    if (err)
-    {
-        printk("file:%s line:%d add_default_rule err:%d\n", __FILE__, __LINE__, err);
-        return err;
-    }
-
-    err = add_test_rule(cht);
-    if (err)
-    {
-        printk("file:%s line:%d add_test_rule err:%d\n", __FILE__, __LINE__, err);
-        destroy_mlx5e_conntrack_table(cht);
-        cht = NULL;
-        return err;
-    }
-
+err:
+    mlx5_destroy_flow_table(ft);
     return err;
 }
 
 
 static void __exit conntrack_fastpath_exit(void)
 {
-    destroy_mlx5e_conntrack_table(cht);
-    cht = NULL;
+    u64 bytes, packets, lastuse;
+    struct mlx5_fc_cache c;
+	c = fc->cache;
+	bytes = c.bytes - fc->lastbytes;
+	packets = c.packets - fc->lastpackets;
+	lastuse = c.lastuse;
+	fc->lastbytes = c.bytes;
+	fc->lastpackets = c.packets;
+    printk("bytes:%llu packets:%llu lastuse:%llu\n", bytes, packets, lastuse);
+
+    mlx5_del_flow_rules(rule);
+    mlx5_destroy_flow_table(ft);
+	ft = NULL;
+
     return ;
 }
 
